896aeaa074f7d11a26874330fc4857390783144f
Li, Yuxuan <lyx776239423@gmail.com>
2026-01-16 20:23:44 +0800

    完整Section 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+
+export class App implements OnInit {
+
+  private httpClient = inject(HttpClient);
+  protected members = signal<any>([]);
+  protected title = 'client';
+
+  ngOnInit(): void {
+    this.httpClient.get('https://localhost:5001/api/Members').subscribe({
+      next: resp => this.members.set(resp),
+      error: err => console.log(err),
+      complete: () => console.log('Http Request Completed')
+    });
+  }
 }
