6e079f4a08b668589955c986e68652bbadf11c5d
Li, Yuxuan <lyx776239423@gmail.com>
2026-01-22 09:33:36 +0800

    路由

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Nav } from "../layout/nav/nav";
+import { Nav } from '../layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
+import { Home } from '../features/home/home';
 import { User } from '../types/user';
+import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-
 export class App implements OnInit {
-
   private httpClient = inject(HttpClient);
   protected members = signal<User[]>([]);
   protected title = 'client';
+
+  // get the router
+  protected router = inject(Router);
 
   private accountService = inject(AccountService);
 
   ngOnInit(): void {
     this.httpClient.get<User[]>('https://localhost:5001/api/Members').subscribe({
-      next: resp => this.members.set(resp),
-      error: err => console.log(err),
-      complete: () => console.log('Http Request Completed')
+      next: (resp) => this.members.set(resp),
+      error: (err) => console.log(err),
+      complete: () => console.log('Http Request Completed'),
     });
 
     this.setCurrentUser();
--- src/layout/nav/nav.ts
+++ src/layout/nav/nav.ts
@@ -1,32 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
-import { signal } from '@angular/core';
+import { LoginCreds } from '../../types/user';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
-
-  protected creds: any = {};
-
+  protected creds = {} as LoginCreds;
 
   protected accountService = inject(AccountService);
+  private router = inject(Router);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (resp) => {
         console.log('login successful', resp);
-        this.creds = {};
+        this.creds = {} as LoginCreds; // clear the form
+        this.router.navigateByUrl('/members');
       },
-      error: (err) => alert('login failed: ' + err.message)
-    })
+      error: (err) => alert('login failed: ' + err.message),
+    });
   }
-   logout() {
+  logout() {
     this.accountService.logout();
   }
-
 }
